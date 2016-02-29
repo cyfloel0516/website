a56b6a36b43e9e7674190b7ac301aa444411176b
Junkai Yang <cyfloel0516@hotmail.com>
2016-02-29 01:01:07 -0500

    Change app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(request, response) {
-  request.send('Hello World!');
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
