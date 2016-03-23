842bb2b823847de7c655b5e10d4ebd154e7d38ef
Junkai Yang <cyfloel0516@hotmail.com>
2016-03-22 21:04:40 -0400

    Adapt new version of home page. Merge some content in the home page for a

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,6 @@ var express = require('express')
   , router = express.Router();
 
 router.use('/', require('./home'))
-
+router.use('/works', require('./works'))
 
 module.exports = router
--- controllers/works.js
+++ controllers/works.js
@@ -2,8 +2,7 @@ var express = require('express')
   , router = express.Router()
 
 router.get('/', function (req, res) {
-  //res.send("12345");
-  res.render('home');
+  res.render('works');
 });
 
 module.exports = router
