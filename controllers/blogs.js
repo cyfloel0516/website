bce3f9f1ee33ec4dd1a30390c53d0a6362b709ea
Junkai Yang <cyfloel0516@hotmail.com>
2016-03-24 21:51:59 -0400

    Add the code plugin to CKEditor. Add mongodb deiver and mondo configuration(mLab)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,4 +5,24 @@ router.get('/edit', function (req, res) {
   res.render('blog_edit');
 });
 
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://cyfloel0516:1990516yjk@ds025239.mlab.com:25239/personal_site');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  	
+  	var kittySchema = mongoose.Schema({
+	    name: String
+	});
+	var Kitten = mongoose.model('Kitten', kittySchema);
+
+	var k = new Kitten({name:'kkk'});
+	k.save();
+
+});
+
 module.exports = router
