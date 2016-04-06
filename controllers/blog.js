b0b66efe5396aef956097a073cd12c41c0d4bf97
Junkai Yang <cyfloel0516@hotmail.com>
2016-04-06 10:50:05 -0400

    Update blog view, remove db.csdonf

--- controllers/blog.js
+++ controllers/blog.js
@@ -13,19 +13,19 @@ router.get('/edit', function (req, res) {
 router.put('/', function(req, res){
 	var db = mongoose.connection;
 	mongoose.connect(dbConfig.mongodb);
-	
+
 	db.once('open', function() {
 	  	var BlogsSchema = new Schema({ content: String, title: String}, 
            { collection : 'blogs' });   // collection name
 		var Blog = mongoose.model('blogs', BlogsSchema);
 
 		var blog = new Blog({content: req.body.blog, title:'12345'});
-		blog.save();
-		console.log('finish');
+		blog.save(function(){
+			db.close();
+		});
+		console.info('finish');
 	});
 });
-
-
 
 
 
