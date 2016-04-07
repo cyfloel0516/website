4b0f21a3c83b1583025ba40121b281c4782ac7ed
Junkai Yang <cyfloel0516@hotmail.com>
2016-04-07 00:10:27 -0400

    Add the work detail view for edms and captioning system

--- controllers/work.js
+++ controllers/work.js
@@ -9,4 +9,8 @@ router.get('/personal_website_2', function(req, res){
 	res.render('work_details/personal_website_2');
 });
 
+router.get('/captioning_system', function(req, res){
+	res.render('work_details/captioning_system');
+});
+
 module.exports = router
