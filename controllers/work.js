be3c6e4ce93094a5cea3ecc37ea547bb4a95fad0
Junkai Yang <cyfloel0516@hotmail.com>
2016-04-07 18:22:13 -0400

    Add work detail page for some other project

--- controllers/work.js
+++ controllers/work.js
@@ -13,4 +13,16 @@ router.get('/captioning_system', function(req, res){
 	res.render('work_details/captioning_system');
 });
 
+router.get('/edms', function(req, res){
+	res.render('work_details/edms');
+});
+
+router.get('/personal_website_1', function(req, res){
+	res.render('work_details/personal_website_1');
+});
+
+router.get('/calendar', function(req, res){
+	res.render('work_details/calendar');
+});
+
 module.exports = router
