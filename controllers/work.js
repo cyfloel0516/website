c0424d06aa2d3e262da4dd18fc4ae92115e44a1b
Junkai Yang <cyfloel0516@hotmail.com>
2016-04-06 21:11:49 -0400

    Add work detail view. Improve the work list view, add hover and links to it.

--- controllers/work.js
+++ controllers/work.js
@@ -5,4 +5,8 @@ router.get('/', function (req, res) {
   res.render('works');
 });
 
+router.get('/personal_website_2', function(req, res){
+	res.render('work_details/personal_website_2');
+});
+
 module.exports = router
